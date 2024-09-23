87c5793a1d2bb093985eab8e692dc7884b49439a
RASHAD K <rameesmonk@Rashads-MacBook-Air.local>
2024-09-23 13:34:36 +0530

    modify package and App file

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     <div>
       <Routes>
         <Route
-          path="/"
+          path="/Hiring-Challenge"
           element={
             <React.Suspense
               fallback={
